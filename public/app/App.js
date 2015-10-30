9eb13bc47f93ec88c730ae72b1d88f04670f7b6b
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-10-30 15:43:59 -0600

    working index

---
+++ public/app/App.js
@@ -0,0 +1,37 @@
+var App = angular.module('theQ', ['ngRoute','ngMaterial'])
+.config(function($routeProvider, $mdThemingProvider){
+
+
+
+
+  $routeProvider
+    .when('/home', {
+
+    })
+
+    .otherwise({
+      redirectTo: '/home'
+    }),
+
+
+
+  $mdThemingProvider.theme('default') .primaryPalette('pink') .accentPalette('orange');
+})
+
+              //   'default': '100',
+              //   'hue-1': '300',
+              //   'hue-2': '600',
+              //   'hue-3': '900'
+              // })
+              // .backgroundPalette('blue', {
+              //   'default': '50',
+              //   'hue-1': '100',
+              //   'hue-2': '600',
+              //   'hue-3': 'A100'
+              // })
+              // .accentPalette('black', {
+              //   'default': '300',
+              //   'hue-1': '100',
+              //   'hue-2': '600',
+              //   'hue-3': 'A100'
+              // });
