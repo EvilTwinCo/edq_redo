aa529b3b0adc6947029a50e76b286f6e60b12eb0
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-10-30 11:43:28 -0600

    initial skeleton

---
+++ app/App.js
@@ -0,0 +1,39 @@
+var app = angular.module('dateABook', ['ngRoute','ngMaterial'])
+.config(function($routeProvider, $mdThemingProvider){
+
+
+
+
+  $routeProvider
+    .when('/home', {
+
+    })
+
+    .otherwise({
+      redirectTo: '/home'
+    }),
+
+
+
+  $mdThemingProvider.theme('default')
+              .primaryPalette('pink', {
+                'default': '100',
+                'hue-1': '300',
+                'hue-2': '600',
+                'hue-3': '900'
+              })
+              .backgroundPalette('blue-grey', {
+                'default': '50',
+                'hue-1': '100',
+                'hue-2': '600',
+                'hue-3': 'A100'
+              })
+              .accentPalette('black', {
+                'default': '300',
+                'hue-1': '100',
+                'hue-2': '600',
+                'hue-3': 'A100'
+              });
+
+
+});
