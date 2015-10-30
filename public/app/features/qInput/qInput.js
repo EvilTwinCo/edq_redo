ab4d1da654e1b78dc79de70bc693664945ee4787
Nathan Allen <nlallenster@gmail.com>
2015-10-30 17:23:37 -0600

    directive and basic view built, not styled

---
+++ public/app/features/qInput/qInput.js
@@ -0,0 +1,16 @@
+angular.module('theQ').directive('qInput', function(){
+    return {
+      templateUrl: 'app/features/qInput/qInputTmpl.html',
+      controller: 'qInputCtrl',
+      controllerAs: 'is',
+      bindToController: true,
+      attribute: 'E',
+      scope:{
+
+      }
+    }
+
+
+
+
+});
