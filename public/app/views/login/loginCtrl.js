b23c35bab0a58ca4cd1fcab4164a9765e9eb98d4
samson <samsonbrimnelson@gmail.com>
2015-11-03 11:43:35 -0700

    Setting up Auth

--- public/app/views/login/loginCtrl.js
+++ public/app/views/login/loginCtrl.js
@@ -1,5 +1,13 @@
-angular.module('theQ').controller('loginCtrl', function() {
-    
-    
-    
+angular.module('theQ').controller('loginCtrl', function($q, $http) {
+
+  this.loginWithPassportDevMtn = function() {
+     var deferred = $q.defer();
+     $http.get('http://localhost:8080/auth/devmtn').then(function(res) {
+         deferred.resolve(res);
+     }, function (err) {
+         deferred.reject('PassportDevMtn login error')
+     })
+     return deferred.promise;
+ }
+  this.loginWithPassportDevMtn();
 });
--- server/server.js
+++ server/server.js
@@ -9,16 +9,16 @@ var app = express();
 var httpServer = require('http').Server(app);
 var SocketIOServer = require('socket.io');
 var ioServer = new SocketIOServer(httpServer);
+var Devmtn = require('devmtn-auth');
+var DevmtnStrategy = Devmtn.Strategy;
+var User = require('./models/User.js')
 
 
 
-
-/* --- UNCOMMENT ONCE WE HAVE MONGO CONTROLLERS ---
-var questionsCtrl = require('./controllers/questionsCtrl.js');
+// var questionsCtrl = require('./controllers/questionsCtrl.js');
 var usersCtrl = require('./controllers/usersCtrl.js');
-var authCtrl = require('./controllers/authCtrl.js');
-var passportDevMtnCtrl = require('./controllers/passportDevMtnCtrl.js');
-*/
+// var authCtrl = require('./controllers/authCtrl.js');
+
 
 var serverPort = 8080;
 var mongoURI = 'mongod://localhost:27017/theQ';
@@ -31,36 +31,41 @@ var ConfidenceCtrl = require('./controllers/ConfidenceCtrl');
 
 var corsWhiteList = ['http://localhost:' + serverPort];
 var corsOptions = {
-    origin: function (origin, callback) {
-        if (corsWhiteList.indexOf(origin) !== -1) callback(null, true);
-        else callback(null, false);
-    }
+  origin: function(origin, callback) {
+    if (corsWhiteList.indexOf(origin) !== -1) callback(null, true);
+    else callback(null, false);
+  }
 }
 
 app.use(express.static(__dirname + '/../public'));
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.options(cors(corsOptions));
 
-/* --- UNCOMMENT ONCE WE HAVE AUTH/PASSPORT SET UP ---
-var SESSION_SECRET = 'THIS NEEDS TO BE CHANGED AND SHOULD BE AN ENV VARIABLE';
+// /* --- UNCOMMENT ONCE WE HAVE AUTH/PASSPORT SET UP ---
+var SESSION_SECRET = process.env.DM_SESSION;
 app.use(session({
-    secret: SESSION_SECRET,
-    name: 'theQCookie.sid',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge: 1000*60*30}, //30 minutes
-    store: new MongoStore({
-        collection: 'connect-mongoSessions',
-        autoRemove: 'native',
-        mongooseConnection: mongoose.connection
-    })
+  secret: SESSION_SECRET,
+  name: 'theQCookie.sid',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 30
+  }, //30 minutes
+  store: new MongoStore({
+    collection: 'connect-mongoSessions',
+    autoRemove: 'native',
+    mongooseConnection: mongoose.connection
+  })
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-*/
+
 
 // ENDPOINTS
 
@@ -80,34 +85,96 @@ app.get('/learningobjective/:_id', LearningObjectiveController.readOne);
 app.put('/learningobjective/:_id', LearningObjectiveController.update);
 app.delete('/learningobjective/:_id', LearningObjectiveController.delete);
 
-/* --- UNCOMMENT ONCE WE HAVE AUTH/PASSPORT SET UP ---
-passport.serializeUser(function(user, done) {done(null, user);});
-passport.deserializeUser(function(user, done) {done(null, user);});
-*/
+app.get('/auth/devmtn', passport.authenticate('devmtn'), function(req, res) {
+  // redirects, so this doesn't get called
+})
+
+// failureRedirect and res.redirect should be changed to match your app
+app.get('/auth/devmtn/callback', passport.authenticate('devmtn', {
+    failureRedirect: '/'
+  }),
+  function(req, res) {
+    //successful authentication
+
+    console.log(req.user); // req.user is created by passport from the decoded json web token
+    console.log('user roles:', req.user.roles);
+    console.log('student:', Devmtn.checkRoles(req.user, 'student')) //example of checking user roles
+
+    res.redirect('/#/studentDashboard')
+  });
+
+app.get('/auth/devmtn/logout', function(req, res) {
+  req.logout();
+  console.log(req.user); //just showing req.user is undefined after logout
+  res.redirect('/#/logout')
+})
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id)
+})
+
+passport.deserializeUser(function(user, done) {
+
+  User.findById(_id: user._id}, function(err, user) {
+    if (err) {
+      return done(err)
+    }
+    return done(null, user)
+  })
+});
+
+passport.use('devmtn', new DevmtnStrategy({
+  app: process.env.DM_APP,
+  client_token: process.env.DM_AUTH,
+  callbackURL: process.env.DM_CALLBACK,
+  jwtSecret: process.env.DM_SECRET,
+}, function(jwtoken, user, done) {
+    req.session.jwtoken = jwtoken
+
+  User.findById({email: user.email}, function(err, result) {
+    if (err) {
+      return done(err)
+    } else {
+      if (result !== undefined) {
+        return done(null, user)
+
+      } else {
+          User.create({email: user.email}, function(err, result) {
+            if (err) {
+              return done(err)
+            } else {
+              return done(null, user)
+          }
+        });
+      }
+    }
+  });
+}))
+
 
 // SOCKET.IO EVENT LISTENERS/DISPATCHERS
 ioServer.use(function(socket, next) {
-    console.log('middleware opportunity... could use for auth');
-    next();
+  console.log('middleware opportunity... could use for auth');
+  next();
 });
 
 ioServer.on('connection', function(socket) {
-    console.log('a user connected');
+  console.log('a user connected');
 
-    socket.on('disconnect', function() {
-        console.log('a user disconnected');
-    });
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+  });
 
-    socket.on('submit confidence', ConfidenceCtrl.handleSubmitConfidence.bind(null, socket, ioServer));
+  socket.on('submit confidence', ConfidenceCtrl.handleSubmitConfidence.bind(null, socket, ioServer));
 
-    socket.on('instructor login', ConfidenceCtrl.handleInstructorLogin.bind(null, socket));
+  socket.on('instructor login', ConfidenceCtrl.handleInstructorLogin.bind(null, socket));
 });
 
 mongoose.set('debug', true);
 mongoose.connect(mongoURI, function() {
-    console.log('Connected to MongoDB: ' + mongoURI);
+  console.log('Connected to MongoDB: ' + mongoURI);
 })
 
 httpServer.listen(serverPort, function() {
-    console.log("Server listening on port: " + serverPort);
+  console.log("Server listening on port: " + serverPort);
 });
