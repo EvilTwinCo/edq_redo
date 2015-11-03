606407fd6a2b194517ebfdfbf6b335b514a79c28
Brack Carmony <brack.carmony@gmail.com>
2015-11-03 12:05:29 -0700

    Tying server into mentor Queue

--- public/app/features/mentorQueue/mentorQueueCtrl.js
+++ public/app/features/mentorQueue/mentorQueueCtrl.js
@@ -1,4 +1,4 @@
-angular.module('theQ').controller('mentorQueueCtrl',function(socketIoSrvc){
+angular.module('theQ').controller('mentorQueueCtrl',function(socketIoSrvc, $scope){
 
 
 
@@ -7,12 +7,15 @@ var socket = socketIoSrvc.getSocket();
 this.questions = [];
 
 socket.on('questionForQueue', function(data){
+  console.log("data Recieved", data);
+  console.log(this);
   this.questions.push(data);
-})
+  $scope.$apply();
+}.bind(this));
 
 socket.on('getAllQuestionsAsked', function(data){
   this.questions = data;
-})
+}.bind(this));
 
 
 this.timeObject = function(){
@@ -45,15 +48,15 @@ this.timeObject = function(){
 
 
 //dummy data
- //  this.question = [
- //
- //    {name:'bob', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie shoes??", solution: '', mentorName: 'MARK'},
- //    {name:'joe', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie knots??", solution: '', mentorName: 'MARK'},
- //    {name:'mary', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie ties??", solution: '', mentorName: ''},
- //    {name:'paul', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie the knote??", solution: '', mentorName: 'MARK'},
- //    {name:'Ringo', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie a bow??", solution: '', mentorName: 'MARK'}
- //
- // ]
+  this.questions = [
+
+    {name:'bob', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie shoes??", solution: '', mentorName: 'MARK'},
+    {name:'joe', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie knots??", solution: '', mentorName: 'MARK'},
+    {name:'mary', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie ties??", solution: '', mentorName: ''},
+    {name:'paul', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie the knote??", solution: '', mentorName: 'MARK'},
+    {name:'Ringo', pictureUrl: 'http://myprofile.bryanschauerte.com/app/images/IMG_1067.jpg', question:"how do I tie a bow??", solution: '', mentorName: 'MARK'}
+
+ ]
 
 //endter q, mentorbegins help, leaveQ
 
--- server/controllers/QuestionCtrl.js
+++ server/controllers/QuestionCtrl.js
@@ -2,9 +2,18 @@ var Question = require('../models/Question');
 
 module.exports = {
   handleStudentQuestionSubmit:function(ioServer, data){
+    //
+    data.name = "I am a fake person. We need to figure out Auth. Hopefully David and Samson are having a good time of it."
+    data.timeWhenEntered = new Date();
     console.log(data);
+    console.log(Question);
     Question.create(data, function(err, newQuestion){
-      ioServer.to('instructors').emit('questionForQueue', newQuestion);
+      if(err){
+        console.log (err);
+      }
+        console.log("No DB error, send info to mentors!");
+        ioServer.emit('questionForQueue', newQuestion);
+
     });
   }
 }
