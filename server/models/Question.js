d5388d0f82fb33f20a0f6f5c4173019c8e762f15
Brack Carmony <brack.carmony@gmail.com>
2015-11-03 10:08:00 -0700

    Intial Schema for Questions

---
+++ server/models/Question.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  mentorName: {
+    type: String
+  },
+  imageUrl: {
+    type: String
+  },
+  question:{
+    type:String,
+    required:true
+  },
+  timeWhenEntered:{
+    type:Date,
+    required:true
+  },
+  timeMentorBegins:{
+    type:Date
+  },
+  timeQuestionAnswered:{
+    type:Date
+  },
+  soulutions:[{
+    solution:String,
+    public:Boolean,
+    name:String
+  }]
+});
+
+module.exports = mongoose.model('Question', schema);
