e2d1ea9460a85614a433457e995450282e2ce396
samson <samsonbrimnelson@gmail.com>
2015-11-05 10:25:03 -0700

    Attendace Tracker

--- public/app/views/login/loginCtrl.js
+++ public/app/views/login/loginCtrl.js
@@ -1,6 +1,5 @@
-angular.module('theQ').controller('loginCtrl', function($q, $http, $window) {
+angular.module('theQ').controller('loginCtrl', function($q, $scope, $http, $window) {
 
-<<<<<<< HEAD
   this.loginWithPassportDevMtn = function() {
      var deferred = $q.defer();
      $http.get('http://localhost:8080/auth/devmtn').then(function(res) {
@@ -12,24 +11,29 @@ angular.module('theQ').controller('loginCtrl', function($q, $http, $window) {
  }
   this.loginWithPassportDevMtn();
 
+//
+// 
+//     this.loginWithPassportDevMtn = function() {
+//
+//         $window.location.href = 'http://localhost:8080/auth/devmtn';
+//
+//         /*
+//         var deferred = $q.defer();
+//
+//         $http.get('http://localhost:8080/auth/devmtn').then(function(res) {
+//             deferred.resolve(res);
+//         }, function (err) {
+//             deferred.reject('PassportDevMtn login error')
+//         })
+//
+//         return deferred.promise;*/
+//     }
+//
+//     this.loginWithPassportDevMtn();
 
-=======
-    this.loginWithPassportDevMtn = function() {
+  $scope.showAttendance = function() {
+    console.log(document.getElementById('attendance-button'))
+    document.getElementById('attendance-button').className += 'active';
+  };
 
-        $window.location.href = 'http://localhost:8080/auth/devmtn'; 
-        
-        /*
-        var deferred = $q.defer();
-        
-        $http.get('http://localhost:8080/auth/devmtn').then(function(res) {
-            deferred.resolve(res);
-        }, function (err) {
-            deferred.reject('PassportDevMtn login error')
-        })
-        
-        return deferred.promise;*/
-    }
-  
-    this.loginWithPassportDevMtn();
->>>>>>> 6b9596d22d9dd67d6c3f3fb1010f69f7dd38761c
 });
