9bc123253b6a3da40a864ecef540c520e8298ce4
David Giles <jairuzu@gmail.com>
2015-11-06 16:41:14 -0700

    still working on confidenceLiveChart

--- public/app/features/confidenceLiveChart/confidenceLiveChart.js
+++ public/app/features/confidenceLiveChart/confidenceLiveChart.js
@@ -1,15 +1,12 @@
-var app = angular.module('theQ')
+var app = angular.module('theQ').directive("confidenceLiveChart", function () {
+    return {
+        templateUrl: "app/features/confidenceLiveChart/confidenceLiveChartTmpl.html",
+        controller: "confidenceLiveChartCtrl",
+        controllerAs: 'is',
+        bindToController: true,
+        attribute: "E",
+        scope: {
 
-app.directive("confidenceLiveChart", function(){
-  return {
-
-    templateUrl:"app/features/confidenceLiveChart/confidenceLiveChartTmpl.html",
-    controller:"confidenceLiveChartCtrl",
-    controllerAs:'is',
-    bindToController:true,
-    attribute:"E",
-    scope:{
-
+        }
     }
-  }
 })
--- public/app/features/confidenceLiveChart/confidenceLiveChartCtrl.js
+++ public/app/features/confidenceLiveChart/confidenceLiveChartCtrl.js
@@ -1,32 +1,32 @@
-var app = angular.module("theQ");
+var app = angular.module("theQ").controller("confidenceLiveChartCtrl", function ($scope, socketIoSrvc) {
 
-app.controller("confidenceLiveChartCtrl", function($scope, socketIoSrvc){
-  var socket = socketIoSrvc.getSocket();
-  $scope.objData = {};
-  socket.emit('instructor login');
-  socket.on('report confidence', function(data){
+    var socket = socketIoSrvc.getSocket();
+    $scope.objData = {};
+    socket.emit('instructor login');
+    socket.on('report confidence', function (data) {
 
-    $scope.objData = data;
-    cleanData();
+        $scope.objData = data;
+        cleanData();
 
-    $scope.$apply();
-  });
-  socket.on('report confidence single', function (data){
-    if ($scope.objData[data.objective_id]){
-      $scope.objData[data.objective_id][data.socketId] = data.value;
-    }else{
-      var temp = {};
-      temp[data.socketId] = data.value;
-      $scope.objData[data.objective_id] = temp;
+        $scope.$apply();
+    });
+    socket.on('report confidence single', function (data) {
+        console.log('recieved report confidence single', data);
+        if ($scope.objData[data.objective_id]) {
+            $scope.objData[data.objective_id][data.socketId] = data.value;
+        } else {
+            var temp = {};
+            temp[data.socketId] = data.value;
+            $scope.objData[data.objective_id] = temp;
+        }
+        cleanData();
+
+        $scope.$apply();
+    });
+
+    function cleanData() {
+        $scope.data = _.mapObject($scope.objData, function (object, index) {
+            return _.values(object);
+        })
     }
-    cleanData();
-
-    $scope.$apply();
-  });
-
-  function cleanData(){
-    $scope.data = _.mapObject($scope.objData, function(object, index){
-      return _.values(object);
-    })
-  }
 });
