e0159741afa2a26fbc4834ece363217496e3b950
Brack Carmony <brackcarmony@gmail.com>
2015-11-06 20:52:00 -0700

    Update studentQueueInfoCtrl.js

--- public/app/features/studentQueueInfo/studentQueueInfoCtrl.js
+++ public/app/features/studentQueueInfo/studentQueueInfoCtrl.js
@@ -3,13 +3,13 @@ angular.module('theQ').controller('studentQueueInfoCtrl', function (socketIoSrvc
     
     socket.on('position in queue', function (index) {
         this.position = index;
-        console.log(index);
+        //console.log(index);
         $scope.$apply();
     }.bind(this));
 
     this.removeSelfFromQueue = function () {
         this.question.timeQuestionAnswered = new Date();
-        console.log(this.question);
+        //console.log(this.question);
         socket.emit("studentDropFromQueueTime", this.question.timeQuestionAnswered);
         socket.emit("request question removal", this.question);
         this.done();
