47adbd68c631271148dbf360c9983b24aa41cf28
Brack Carmony <brack.carmony@gmail.com>
2015-11-16 15:56:18 -0700

    Removing excess comments from directives

--- public/app/features/flashPollMonitor/flashPollMonitorCtrl.js
+++ public/app/features/flashPollMonitor/flashPollMonitorCtrl.js
@@ -5,9 +5,9 @@ var app = angular.module("theQ").controller("flashPollMonitorCtrl", function(soc
 
 
   socket.on('flashPoll', function(data) {
-    $scope.dataSet = data
+    $scope.dataSet = data;
     updateResults();
-  })
+  });
 
   $scope.dataSet = [];
   var labels = ['A', 'B', 'C'];
@@ -31,7 +31,7 @@ var app = angular.module("theQ").controller("flashPollMonitorCtrl", function(soc
     .append('text')
     .classed('voteNumbers', true);
 
-  var labels = baseBox.selectAll("labels")
+   labels = baseBox.selectAll("labels")
     .data(labels)
     .enter()
     .append('text')
@@ -39,7 +39,7 @@ var app = angular.module("theQ").controller("flashPollMonitorCtrl", function(soc
       return d;
     })
     .attr('x', function(d, i) {
-      return containerWidth * (.5 + i);
+      return containerWidth * (0.5 + i);
     })
     .attr('y', 98)
     .attr("font-family", "sans-serif")
@@ -48,24 +48,22 @@ var app = angular.module("theQ").controller("flashPollMonitorCtrl", function(soc
     .attr("text-anchor", "middle")
     .classed('labels', true);
 
-  var resultSet;
-
   this.clearData = function() {
     console.log(self.cohortId);
-    socket.emit('removeStudentFlashPollData', self.cohortId)
+    socket.emit('removeStudentFlashPollData', self.cohortId);
     $scope.dataSet = [];
     updateResults();
-  }
+  };
 
   function updateResults() {
     aCount = $scope.dataSet.filter(function(item) {
-      return (item === "A" ? true : false)
+      return (item === "A" ? true : false);
     }).length;
     bCount = $scope.dataSet.filter(function(item) {
-      return (item === "B" ? true : false)
+      return (item === "B" ? true : false);
     }).length;
     cCount = $scope.dataSet.filter(function(item) {
-      return (item === "C" ? true : false)
+      return (item === "C" ? true : false);
     }).length;
 
     resultSet = [aCount, bCount, cCount];
@@ -96,7 +94,7 @@ var app = angular.module("theQ").controller("flashPollMonitorCtrl", function(soc
         return d;
       })
       .attr('x', function(d, i) {
-        return containerWidth * (.5 + i);
+        return containerWidth * (0.5 + i);
       })
       .attr('y', function(d) {
         return 88 - scale(d);
@@ -105,101 +103,4 @@ var app = angular.module("theQ").controller("flashPollMonitorCtrl", function(soc
       .attr('font-size', "10")
       .attr("text-anchor", "middle");
   }
-
-
-
-
-
-
-  // .attr("text-anchor", "middle" )
-
-
-  // end bryans
-
-  //
-  //
-  // $element[0].width = 500;
-  // $element[0].height = 500;
-  //
-  // var dataSet = [1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3];
-  // //var dataSet = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
-  // //var dataSet = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 2, 2, 3, 3, 3];
-  // //var dataSet = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
-  //
-  // var aCount = dataSet.filter(function(item) {return (item === 1 ? true : false)}).length;
-  // var bCount = dataSet.filter(function(item) {return (item === 2 ? true : false)}).length;
-  // var cCount = dataSet.filter(function(item) {return (item === 3 ? true : false)}).length;
-  //
-  // var resultSet = [aCount, bCount, cCount];
-  //
-  // console.log(resultSet);
-  //
-  // var h = $element[0].height;
-  // var w = $element[0].width;
-  // var xPadding = w*.1;
-  // var maxResult = d3.max(resultSet);
-  //
-  // var yScale = d3.scale.linear()
-  //     .domain([0, dataSet.length])
-  //     .range([0, h])
-  //
-  // var chart = d3.select('#svgChart')
-  //     .attr("width", w + "%")
-  //     .attr("height", h)
-  //     .style({
-  //         "border-bottom": "1px solid black"
-  //     });
-  //
-  // var dataBars = chart.selectAll('rect')
-  //     .data(resultSet)
-  //     .enter()
-  //     .append('rect')
-  //     .attr({
-  //         "x": function (d, i) {
-  //             return (xPadding/2 + i*(w/resultSet.length));
-  //         },
-  //         "y": function (d) {
-  //             return (h - yScale(d));
-  //         },
-  //         "width": function(d) {
-  //             return (w/resultSet.length-xPadding);
-  //         },
-  //         "height": function(d) {
-  //             return yScale(d);
-  //         }
-  //     })
-  //     .style({
-  //         "fill": function (d) {
-  //             return d3.rgb(0, (d/dataSet.length)*255, 0);
-  //         }
-  //     })
-  //
-  // var labels = chart.selectAll('text')
-  //     .data(resultSet)
-  //     .enter()
-  //     .append('text')
-  //     .text(function(d) {
-  //         return d;
-  //     })
-  //     .attr({
-  //         "x": function (d, i) {
-  //             return (xPadding/2 + i*(w/resultSet.length) + (w/resultSet.length-xPadding)/2);
-  //         },
-  //         "y": function (d) {
-  //             if (d > .8*dataSet.length) return (h - yScale(d) + 40);
-  //             else return (h - yScale(d) - 10);
-  //         },
-  //         "font-family": "sans-serif",
-  //         "font-size": "40px",
-  //         "text-anchor": "middle"
-  //     })
-  //     .style({
-  //         "fill": function (d) {
-  //             /*if (d > .8*dataSet.length) return "white";
-  //             else return "black"*/
-  //             return "black"
-  //         }
-  //     });
-  //
-  //
 });
