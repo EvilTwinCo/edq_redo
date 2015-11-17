7ba6dff305d09e5ff61defaab4d6a809578a8457
jairuzu <jairuzu@gmail.com>
2015-11-16 17:01:34 -0700

    Update FlashPollCtrl.js

--- server/controllers/FlashPollCtrl.js
+++ server/controllers/FlashPollCtrl.js
@@ -12,7 +12,6 @@ module.exports = {
 
   handleFlashPollRemoval: function(socket, cohortId) {
     studentVotes = [];
-      console.log('flash poll', cohortId);
       socket.server.to('student cohort:'+cohortId).emit('togglePolls', true);
   }
 
