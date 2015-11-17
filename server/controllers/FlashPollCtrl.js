bff4edd623fff9e1b9f648e5f71b4c1115863e57
David Giles <jairuzu@gmail.com>
2015-11-16 20:45:59 -0700

    adding sticky flash-poll capability

--- server/controllers/FlashPollCtrl.js
+++ server/controllers/FlashPollCtrl.js
@@ -2,17 +2,37 @@ var passportSocketIo = require('passport.socketio');
 
 
 var studentVotes = [];
+var openCohortPolls = {};
 
 module.exports = {
 
   handleFlashPollSubmit: function(socket, data) {
-    studentVotes.push(data);
+      var cohortId = socket.request.user.devMtn.cohortId;
+      var studentId = socket.request.user.devMtn.id;
+      studentVotes.push(data);
     socket.server.to('instructors').emit('flashPoll', studentVotes);
+      openCohortPolls[cohortId].push(studentId);
   },
 
   handleFlashPollRemoval: function(socket, cohortId) {
-    studentVotes = [];
+        studentVotes = [];
+        openCohortPolls[cohortId] = [];
+      setTimeout(removeOpenPoll.bind(null, cohortId), 1000*60*5);
       socket.server.to('student cohort:'+cohortId).emit('togglePolls', true);
-  }
-
+  },
+    
+    handleFlashPollGetStatus: function(socket) {
+        var cohortId = socket.request.user.devMtn.cohortId;
+        var studentId = socket.request.user.devMtn.id;
+        if (openCohortPolls[cohortId]) {
+            if (openCohortPolls[cohortId].indexOf(studentId) === -1) {
+                socket.server.to('student cohort:'+cohortId).emit('togglePolls', true);
+            }
+        }
+    }
 };
+
+function removeOpenPoll(cohortId) {
+    console.log('deleting openCohortPolls['+cohortId+']')
+     delete openCohortPolls[cohortId]
+}
