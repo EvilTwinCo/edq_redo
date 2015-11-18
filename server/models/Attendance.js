23206140a1d4a2b1ac993273a11e85a04d5c9b4e
David Giles <jairuzu@gmail.com>
2015-11-18 13:03:47 -0700

    fixing attendance database pulling

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -2,32 +2,15 @@ var mongoose = require('mongoose');
 
 var attendancesSchema = new mongoose.Schema({
 
-  user: {
-    type: mongoose.Schema.Types.ObjectId
-    // required: true
-  },
-  cohort: {
-    type: String
-    // required: true
-  },
-
-  attendanceData: {
-
-    timeIn: {
-      type: Date
-    },
-    timeOut: {
-      type: Date
-    },
-    score: {
-      type: Number
-    },
-
-    dateOfAttendance: {
-      type: Date
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    cohortId: {type: String},
+    attendanceData: {
+        timeIn: {type: Date},
+        timeOut: {type: Date},
+        score: {type: Number},
+        dateOfAttendance: {type: Date}
     }
-  }
-
+    
 });
 
 module.exports = mongoose.model('Attendances', attendancesSchema);
