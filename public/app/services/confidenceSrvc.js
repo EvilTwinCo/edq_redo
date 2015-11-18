5a39e8d36e480c2d50446fc13546d5492c0d02af
Brack Carmony <brack.carmony@gmail.com>
2015-11-17 18:26:46 -0700

    Hmmm

--- public/app/services/confidenceSrvc.js
+++ public/app/services/confidenceSrvc.js
@@ -1,34 +1,31 @@
 angular.module('theQ').service('confidenceSrvc', function($q, $http) {
 
-    //var baseUrl = 'http://192/'
-    var baseUrl = 'http://'
-
     this.getConfidences = function(cohortId) {
         var deferred = $q.defer();
 
-        $http.get(baseUrl + 'admin/confidences/' + cohortId).then(function (res) {
+        $http.get('admin/confidences/' + cohortId).then(function (res) {
             //console.log(res);
             deferred.resolve(res.data);
         }, function (err) {
             console.log(err);
             deferred.reject(err);
-        })
+        });
 
         return deferred.promise;
-    }
+    };
 
     this.getUserLearningObjConfidences = function(obj) {
         var deferred = $q.defer();
 
-        $http.get(baseUrl + 'admin/confidences/user/' + obj.userId + '/' + obj.learningObjId).then(function (res) {
+        $http.get('admin/confidences/user/' + obj.userId + '/' + obj.learningObjId).then(function (res) {
             //console.log(res);
             deferred.resolve(res.data);
         }, function (err) {
             console.log(err);
             deferred.reject(err);
-        })
+        });
 
         return deferred.promise;
-    }
+    };
 
 });
--- public/app/services/learningObjSrvc.js
+++ public/app/services/learningObjSrvc.js
@@ -1,19 +1,17 @@
 angular.module('theQ').service('learningObjSrvc', function($q, $http) {
-    
-    var baseURL = "http://"
-    
+
     this.getLearningObjs = function() {
         var deferred = $q.defer();
-        
-        $http.get(baseURL + 'user/learningObj').then(function(res) {
-            
+
+        $http.get('user/learningObj').then(function(res) {
+
             deferred.resolve(res.data);
         }, function(err) {
-            
+
             deferred.reject(err);
         })
-        
+
         return deferred.promise;
     }
-    
+
 });
