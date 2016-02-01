43340533327d94a5f2564bef23606a8f20efd386
Brack Carmony <brackcarmony@gmail.com>
2016-02-01 11:50:25 -0700

    Hide Learning Objectives Ctrl and Curriculumn if there's none for the day.  Added default Objectives for question queue

--- public/app/features/qInput/qInputCtrl.js
+++ public/app/features/qInput/qInputCtrl.js
@@ -3,6 +3,7 @@ angular.module('theQ').controller('qInputCtrl', function (socketIoSrvc) {
     //this.question = this.question;
     var day = [];
     var socket = socketIoSrvc.getSocket();
+    this.defaultObjectives = [{objectiveName:"Javascript"}, {objectiveName:"Angular"},{objectiveName:"Express"},{objectiveName:"Mongo"}];
 
     socket.on('dayObjectives', function (arr) {
         day = arr;
